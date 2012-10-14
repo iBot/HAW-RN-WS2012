6acb3e82562372d4b19834356e28da32a70b6374
iBot <Tobi>
2012-10-14 11:39:38 +0200

    Commit mit Start-Datei

---
+++ HAW-RN-WS2012/src/pop3proxy/Start.java
@@ -0,0 +1,20 @@
+package pop3proxy;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tobi
+ */
+public class Start {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
